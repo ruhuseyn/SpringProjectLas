064b1006d75687411bbb00bec9d3a6904effbef2
ruhuseyn <huseyinovrufet49@gmail.com>
2023-04-25 15:08:10 +0400

    Controller add

---
+++ spring1/src/main/java/com/example/spring1/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.spring1.repository;
+
+import com.example.spring1.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
